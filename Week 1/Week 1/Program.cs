286dda3a2b5848ce65538c39bf8d25627c8ccfd4
RazvanPaul <tudureanu50@gmail.com>
2022-11-15 13:23:10 +0200

    Algoritm calcul

---
+++ Week 1/Week 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Ce suma doriti sa retrageti?");
+string input = Console.ReadLine();
+double suma = Convert.ToInt32(input);
+double procent = 5;
+//double taxa = (double)((suma * procent) / 100);
+if ((double) suma <= 20)
+{
+    Console.WriteLine("Taxa dumneavoasta este 1$");
+}
+else if ((double) suma > 20)
+{
+    double taxa = (double)((suma * procent) / 100);
+    Console.WriteLine("Taxa dumneavoastra este: " + taxa + "$");
+}
+   
+
+
